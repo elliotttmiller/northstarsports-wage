d69cf4b3b96d969fa72762898c7ed4c42ae8c918
elliotttmiller <elliotttmiller@hotmail.com>
2025-09-14 06:37:24 +0000

    Edited Spark

--- src/SimpleTest.tsx
+++ src/SimpleTest.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { Button } from '@/components/ui/button'
 
 
-export function SimpleTest() {
   return (
     <div className="text-center">
       <h1>Simple Test Component</h1>
