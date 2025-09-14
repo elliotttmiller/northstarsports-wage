51f5c017b9419bc8339e6775a4e73e5e4bd1170b
elliotttmiller <elliotttmiller@hotmail.com>
2025-09-14 06:04:44 +0000

    Edited Spark

--- src/SimpleTest.tsx
+++ src/SimpleTest.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Button } from '@/components/ui/button'
+
 
 export function SimpleTest() {
   return (
@@ -9,7 +9,6 @@ export function SimpleTest() {
     </div>
   )
 }
-
 
 
 
